dbb5be934f3e060444a5094fe788f3f947b2f8b4
Gardner Bickford <gardner@bickford.nz>
2025-11-12 14:34:01 +1300

    Cleanup unused files

--- dlcodeql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-codeql pack download codeql/cpp-queries codeql/python-queries codeql/javascript-queries codeql/java-queries codeql/csharp-queries codeql/go-queries codeql/ruby-queries
-
-echo "Downloading CodeQL packs... This may take a while."
-wait -n
